d2c5b98fb82bc90f6f55b38a01cca3f17d476200
Gherase12 <c_gherase12@yahoo.ro>
2023-03-09 00:32:15 +0200

    sdasd

--- fetchers/getPrices.js
+++ fetchers/getPrices.js
@@ -3,8 +3,8 @@ import axios  from 'axios';
  export const fetchPrices = async (amount, one, two, decimals)=>{
     //http://localhost:3000/api/tokenPrice
       const res = await axios.get(
-        `https://www.roswelldex.ai/api/tokenPrice`
-        // `http://localhost:3000/api/tokenPrice`
+        // `https://www.roswelldex.ai/api/tokenPrice`
+        `http://localhost:3000/api/tokenPrice`
         , {
         params: {addressOne: one, addressTwo: two, amount: Number(amount * 10 ** decimals)  }
       }, { method: "GET" })
